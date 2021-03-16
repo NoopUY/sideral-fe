473dc458ed4601be83318aed9c9a6aa81dde0d6b
Daniel <dwaksman@gmail.com>
2021-03-16 10:37:24 -0300

    WIP

---
+++ pages/batch/_id.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    HOLA
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+
+  data: () => {
+    return {
+    };
+  },
+
+  computed: {
+    ...mapGetters({ batches: 'batches/data' })
+  },
+
+  mounted () {
+    console.log(this.$route);
+  },
+
+  methods: {
+    ...mapActions({ fetchBatches: 'batches/fetch' })
+  }
+};
+</script>
