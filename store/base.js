03aa752c3c3020c648a3a65b83bb324a565dbebc
Daniel <dwaksman@gmail.com>
2021-05-10 14:55:19 -0300

    Created a busy property to keep track of api usage

--- store/base.js
+++ store/base.js
@@ -6,7 +6,8 @@ export const mixinState = () => ({
   andFilters: [],
   sort: '',
   where: {},
-  search: ''
+  search: '',
+  busy: false
 })
 
 export const mixinGetters = () => ({
@@ -16,6 +17,7 @@ export const mixinGetters = () => ({
   limit: state => state.limit,
   search: state => state.search,
   andFilters: state => state.andFilters,
+  busy: state => state.busy,
   currentPage: state => state.currentPage
 })
 
@@ -35,12 +37,14 @@ export const mixinMutations = () => ({
   andfilters: (state, andFilters) => { state.andFilters = andFilters },
   and: (state, and) => { state.andFilters.push(and) },
   search: (state, search) => { state.search = search },
-  currentPage: (state, currentPage) => { state.currentPage = currentPage }
+  currentPage: (state, currentPage) => { state.currentPage = currentPage },
+  busy: (state, status) => { state.busy = status }
 })
 
 export const mixinActions = resource => ({
 
   async fetch ({ commit, state }) {
+    commit('busy', true);
     const { payload } = await this.$axios.$get(`/${resource}`, {
       params: {
         where: state.where,
@@ -53,27 +57,36 @@ export const mixinActions = resource => ({
 
     commit('data', payload.data);
     commit('count', payload.total_records);
+    commit('busy', false);
   },
 
-  async fetchById ({ state }, _id) {
+  async fetchById ({ commit, state }, _id) {
+    commit('busy', true);
     const { payload } = await this.$axios.$get(`/${resource}/${_id}`);
+    commit('busy', false);
     return payload
   },
 
   async delete ({ commit }, element) {
-    commit('delete', element)
+    commit('delete', element);
+    commit('busy', true);
     await this.$axios.$delete(`/${resource}/${element._id}`);
+    commit('busy', false);
   },
 
   async add ({ commit, dispatch }, element) {
     commit('add', element);
+    commit('busy', true);
     await this.$axios.$post(`/${resource}`, element)
+    commit('busy', false);
     dispatch('fetch');
   },
 
   async update ({ commit, dispatch }, element) {
+    commit('busy', true);
     commit('update', element)
-    await this.$axios.$put(`/${resource}/${element._id}`, element)
+    await this.$axios.$put(`/${resource}/${element._id}`, element);
+    commit('busy', false);
     dispatch('fetch');
   }
 })
